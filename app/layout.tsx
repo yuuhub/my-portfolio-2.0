93b442cb9fa8ded3796d1fae3e1b88a87eedc4bd
Yuuri Peñas <jon@gethooked360.com>
2023-12-14 16:53:44 +0800

    Add another gradient to hero

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} bg-teal-50 text-slate-zinc`}>{children}</body>
+      <body className={`${inter.className} bg-teal-50 text-slate-950 relative`}>
+        <div className='bg-teal-200 absolute top-[-6rem] -z-10 right-[3rem] h-[31.25rem] w-[40.25rem] rounded-full blur-[10rem]  sm:w-[68.75rem]'></div>
+        <div className='bg-indigo-200 absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem]  sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left[-5rem]'></div>
+        {children}
+      </body>
     </html>
   )
 }
